24fd3fecf8c2ca6e0a094048ec151cf312e14deb
Gulzarokz <gokz22301@gamil.com>
2024-10-16 21:53:21 +0500

    add to the repo of multiple condition and nested if statment

---
+++ index.js
@@ -0,0 +1,100 @@
+// chapter No 13 Check Multiple condition
+
+
+// const firstNumber = +prompt("Enter first Number");
+// const secondNumber = +prompt("Enter first Number");
+// const thirdNumber = +prompt("Enter first Number");
+
+
+// if(firstNumber > secondNumber && firstNumber > thirdNumber){
+//     alert("First number is greatest number");
+// }else if(secondNumber > thirdNumber){
+//     alert("Second number is greater");
+
+// }else{
+//     console.log("The third number this is "+ thirdNumber + " " " is the greatest number");
+    
+// }
+
+
+// const userName = prompt("Enter user Name ");
+// const score = +prompt("Please Enter your score");
+
+// if(score >= 90 ){
+//     alert(userName + "your grade is A");
+// }
+// else if(score >= 80){
+//     alert(userName + "your grade is B");
+// }
+// else if(score >= 70){
+//     alert(userName + "Your grade is C")
+// }
+// else if(score >= 60){
+//     alert(userName + "your grade is D")
+// }
+// else{
+//     alert( "invalid input")
+// }
+// console.log(`you name is ${userName} and your score is = ${score}`);
+
+//   const weight = +prompt("enter a valid weight");
+//   const time = +prompt("enter a time in seconds");
+
+// if (weight > 300 && time < 6) {
+//     alert("Come to our tryout!");
+//     }
+//     else {
+//     alert("Come to our cookout!");
+//     }
+
+//     let weight , time, age, gender;
+//  if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+//     }
+//     else {
+//     alert("Come to our cookout!");
+// //     }
+
+// const age = +prompt("enter age for living")
+
+// if ((age > 65 || age < 21) && res === "U.S.") {
+//     alert("you can live in U.S")
+// }
+// else{
+//     alert("you can live any where ")
+// }
+
+// Chapter No 14 nested if 
+// const userName = prompt("Enter User Name please ");
+// let password = ""
+
+// if (userName === "Admin") {
+//     password = prompt("Please Enter passwrod please");
+//     if(password === "gulzar@123") {
+//         document.write("login seccessfully");
+//     }
+//     else {
+//         document.write("incorrect password! please try again");
+//     }
+// }
+// else 
+// {
+//     document.write("User not found please try again");
+// }
+
+// const age = prompt("Enter Age please ");
+// const isStudent = prompt("Are you student \n0.No\n1.yes");
+
+// if (age < 12) {
+//     document.write("Child Tickets 5$")
+
+// }else{
+//     if (age > 12 && age < 18 && isStudent){
+//         document.write("Student Tacket 10$");
+//     }
+//     else{
+//         document.write("Adult Tackets 15$")
+//     }
+}
+
+
